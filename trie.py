77755b77b1bbc7f8be6ac66ef3ffe59af2e6854b
jming <jming@college.harvard.edu>
2014-10-18 20:25:10 -0400

    able to work on 10 posts
    Former-commit-id: 9094ba81a0b756c4d9b2cd0a14a491fc9f576874
    
    Former-commit-id: af4120985cf416b4f8ff62b58aab25ab68ea7853

--- trie.py
+++ trie.py
@@ -1,14 +1,25 @@
+import pandas as pd
 _end = '_end_'
 
-# def load_chv()
+# def load_chv(file_name):
+# 	return chv_df = pd.read_csv(file_name, sep='\t', names=column_names, usecols=[0,1,2])
 
-def make_trie(words):
+def make_trie(df):
 	root = {}
-	for word in words:
+	for i,row in df.get_group(1).iterrows():
+	# for word in words:
+		# print row
+		# print row['term']
 		current_dict = root
-		for letter in word:
-			current_dict = current_dict.setdefault(letter, {})
-		current_dict = current_dict.setdefault(_end, 12)
+		# print row['term']
+		# if row['term'] == 'nan' or row['term'] == float('nan'):
+		if type(row['term']) is str:
+			# print 'derp'
+			# row['term'] = ['n','a','n']
+			for letter in row['term']:
+				current_dict = current_dict.setdefault(letter, {})
+		current_dict = current_dict.setdefault(_end, row['cui'])
+		# break
 	return root
 
 # print make_trie(['foo', 'bar', 'baz', 'barz'])
@@ -26,6 +37,6 @@ def in_trie(trie, word):
 		else:
 			return -1
 
-t = make_trie(['foo', 'bar', 'baz', 'barz'])
-print t
-print in_trie(t, 'bar')
+# t = make_trie(['foo', 'bar', 'baz', 'barz'])
+# print t
+# print in_trie(t, 'bar')
