9d0c1ea627799bcc76c85c55abfd30ef86171e48
jming <jming@college.harvard.edu>
2014-11-10 16:44:23 -0500

    run on cui matrix
    Former-commit-id: a7b6d4736d41ece85fded82ba34a50d81e931ce3

--- anchor-word-recovery/autism.sh
+++ anchor-word-recovery/autism.sh
@@ -6,14 +6,14 @@ echo 'start autism'
 # python uci_to_scipy.py ../all_posts.txt all_posts.mat
 
 echo 'preprocessing, removing rare words and stop words'
-python truncate_vocabulary.py ../autism_words_T.mat ../autism_vocab.txt 50
+python truncate_vocabulary.py ../autism_counter_T.mat ../autism_cui2.txt 50
 
 for loss in L2
 do
 	for K in 20 50 100
 	do
 		echo "learning with nonnegative recover method using $loss loss..."
-		python learn_topics.py ../autism_words_T.mat.trunc.mat settings.example ../autism_vocab.txt.trunc $K $loss autism\_out.$K
+		python learn_topics.py ../autism_counter_T.mat.trunc.mat settings.example ../autism_cui2.txt.trunc $K $loss cui2\_out.$K
 	done
 done
 
