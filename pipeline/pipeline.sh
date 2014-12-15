04baf6542cd58f63c593202df2bfc839c925d001
jming <jming@college.harvard.edu>
2014-12-15 15:20:49 -0500

    pipeline works
    Former-commit-id: 4d3786d131cc0ef14b65f34fac254b1c726f48c6

--- pipeline/pipeline.sh
+++ pipeline/pipeline.sh
@@ -4,14 +4,14 @@ echo 'start pipeline'
 
 echo 'processing posts'
 # python get_q/getq.py all_posts.txt chv.tsv cui_counter.mat cui_list.txt
-cd get_q
-ls 
-python getq.py test.txt chv_small.csv ../cui_counter.mat ../cui_list.txt
-cd ..
+python get_q/getq.py get_q/test.txt get_q/chv_small.csv cui_counter.mat cui_list.txt
 
+echo 'starting words recovery'
 for loss in L2
 do
 	for K in 20 50 100
 	do
 		echo 'learning with nonnegative recover method using $loss loss...'
-		python anchor_words_recovery/learn_topics.py cui_counter.mat anchor_words_recovery/settings.example cui_list.txt $K $loss result\_out.$K
+		python get_anchors/learn_topics.py cui_counter.mat get_anchors/settings.example cui_list.txt $K $loss result\_out.$K
+	done
+done
