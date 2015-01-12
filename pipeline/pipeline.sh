da5eefa61814448691869bcceb8f43536e45146e
jming <jming@college.harvard.edu>
2015-01-12 15:03:35 -0500

    run likelihoods
    Former-commit-id: 2290f737afb66db67a82d360f2741236ba20ba11

--- pipeline/pipeline.sh
+++ pipeline/pipeline.sh
@@ -1,20 +1,20 @@
 #!/bin/bash
 
-# echo 'start pipeline'
+echo 'start pipeline'
 
-# echo 'processing posts'
-# # python get_q/getq.py all_posts.txt chv.tsv cui_counter.mat cui_list.txt
-# # python get_q/getq.py get_q/test.txt get_q/chv_small.csv cui_counter.mat cui_list.txt
-# # python get_q/getq.py get_q/test.txt get_q/chv.tsv test_counter.mat test_list.txt
-# # python get_q/getq.py get_q/joy_all_posts.txt get_q/chv.tsv cui_counter.mat cui_list.txt
-# python get_q/getq.py get_q/joy_all_posts.txt get_q/mrsty_filtered.csv cui_counter.mat cui_list.txt
+echo 'processing posts'
+# python get_q/getq.py all_posts.txt chv.tsv cui_counter.mat cui_list.txt
+# python get_q/getq.py get_q/test.txt get_q/chv_small.csv cui_counter.mat cui_list.txt
+# python get_q/getq.py get_q/test.txt get_q/chv.tsv test_counter.mat test_list.txt
+# python get_q/getq.py get_q/joy_all_posts.txt get_q/chv.tsv cui_counter.mat cui_list.txt
+python get_q/getq.py get_q/joy_all_posts.txt get_q/mrsty_filtered.csv cui_counter.mat cui_list.txt
 
 
 # echo 'preprocessing, removing rare words and stop words'
 # # # python get_anchors/truncate_vocabulary.py test_counter.mat test_list.txt 50
 # python get_anchors/truncate_vocabulary.py cui_counter.mat cui_list.txt 50
 
-# # echo 'starting words recovery'
+# echo 'starting words recovery'
 # for loss in L2
 # do
 # 	for K in 20 50 100
@@ -22,14 +22,14 @@
 # 		echo 'learning with nonnegative recover method using $loss loss...'
 # 		# python get_anchors/learn_topics.py cui_counter.mat get_anchors/settings.example cui_list.txt $K $loss result\_out.$K
 # 		# python get_anchors/learn_topics.py test_counter.mat.trunc.mat get_anchors/settings.example test_list.txt.trunc $K $loss result\_out.$K
-# 		python get_anchors/learn_topics.py cui_counter.mat.trunc.mat get_anchors/settings.example cui_list.txt.trunc $K $loss result6/result\_out.$K
+# 		python get_anchors/learn_topics.py cui_counter.mat.trunc.mat get_anchors/settings.example cui_list.txt.trunc $K $loss result7/result\_out.$K
 
 # 	done
 # done
 
-echo 'starting translation'
-for K in 20 50 100
-do
-	echo 'translating $K'
-	python get_result/translate.py result5/result\_out.$K.topwords result5/result\_out.$K.topwords.translate cui_dict.txt
-done
+# echo 'starting translation'
+# for K in 20 50 100
+# do
+# 	echo 'translating $K'
+# 	python get_result/translate.py result7/result\_out.$K.topwords result7/result\_out.$K.topwords.translate cui_dict.txt
+# done
