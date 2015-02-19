e68cfad09e3e4d08fea49df837e7bd522996aace
jming <jming@college.harvard.edu>
2015-02-18 22:25:04 -0500

    updated version
    Former-commit-id: 7191317b93a8d0aa1f6ee3729ba301fb9e1754bb

--- pipeline/get_anchors/gram_schmidt_convhull.py
+++ pipeline/get_anchors/gram_schmidt_convhull.py
@@ -12,13 +12,16 @@ def Projection_Find(M_orig, r, candidates, var):
     reps = 1
     configs = 1
 
-    n = M_orig[:, 0].size
+    dim_n = M_orig[:, 0].size
     dim = M_orig[0, :].size
+
+    # print n,dim
 
     M = M_orig.copy()
     
     # stored recovered anchor words
-    anchor_sets = np.zeros((reps*configs+1, r))
+    anchor_sets = np.zeros((reps*configs*r, r))
+    # print anchor_sets.shape
 
     # store the basis vectors of the subspace spanned by the anchor word vectors
     basis = np.zeros((r-1, dim))
@@ -82,9 +85,10 @@ def Projection_Find(M_orig, r, candidates, var):
 
             # previous set number
             set_n = rep*(r-1)+j
+            print 'setn', set_n
 
             # create config table for storing
-            config_table = np.zeros((configs, n, dim))
+            config_table = np.zeros((configs, dim_n, dim))
             config_avgs = np.zeros((configs, len(candidates)))
 
             for n in range(0, configs):
@@ -98,12 +102,12 @@ def Projection_Find(M_orig, r, candidates, var):
                 np.savetxt('log.config'+str(n), config)
 
                 # try replacing anchor j in configuration n
-                for i in candidates:
+                for i,candidate in enumerate(candidates):
 
                     print 'candidate', i, time.time()
 
                     # try using candidate basis
-                    active_set = np.concatenate((anchor_sets[set_n][:j], [i], anchor_sets[set_n][j+1:]))
+                    active_set = np.concatenate((anchor_sets[set_n][:j], [candidate], anchor_sets[set_n][j+1:]))
 
                     # count number of points included in this configuration
                     # TODO: should this be counting how many from the old config are in the new config???
@@ -113,13 +117,18 @@ def Projection_Find(M_orig, r, candidates, var):
                     print count
                     config_avgs[n][i] = count
 
+                    np.savetxt('log.anchor_sets', anchor_sets)
+
             # find the averages of all configurations across candidates
-            max_incl = np.argmax(config_avgs.sum(axis=0))
+            max_incl = candidates[np.argmax(config_avgs.sum(axis=0))]
             # max_incl = candidates[max(config_avgs[n])]
 
             # replace with the anchor with the highest average
+            # print anchor_sets.shape
+            # print set_n, j, max_incl, set_n
             anchor_sets[set_n+1] = np.concatenate((anchor_sets[set_n][:j], [max_incl], anchor_sets[set_n][j+1:]))
             print anchor_sets[set_n+1]
+            np.savetxt("log.anchor_sets", anchor_sets)
                 
     # TODO: test the candidate configurations before selecting anchor_indices
 
@@ -129,11 +138,12 @@ def Projection_Find(M_orig, r, candidates, var):
 
     # convert numpy array to python list
     anchor_indices = anchor_sets[-1]
+    print anchor_indices
     anchor_words = [M_orig[x] for x in anchor_indices]
+    anchor_indices_list = [ai for ai in anchor_indices]
 
-    anchor_indices_list = []
-    for i in range(r):
-        anchor_indices_list.append(anchor_indices[i])
+    # for i in range(r):
+        # anchor_indices_list.append(anchor_indices[i])
     
     return (anchor_words, anchor_indices_list)
 
@@ -143,7 +153,7 @@ def create_config(M, var):
 
 def in_conv_hull(b, p):
 
-    print b.shape, p.shape
+    # print b.shape, p.shape
 
     def mfunc(x):
         return np.dot(x,b) - p
@@ -171,9 +181,9 @@ def in_conv_hull(b, p):
     res = scipy.optimize.minimize(func, x0, constraints=cons, bounds=bds)
 
     # print 
-    print res
+    # print res
     # print b, p
-    print np.linalg.norm(mfunc(res.x), ord='fro')**2
+    # print np.linalg.norm(mfunc(res.x), ord='fro')**2
     return np.linalg.norm(mfunc(res.x), ord='fro')**2 <= 1e-7
     # print np.linalg.norm(np.dot(res.x , b) - p) ** 2
 
@@ -210,7 +220,7 @@ def count_incl(config, anchor_set):
     for point in config:
         incl.append(in_conv_hull(anchors, point))
 
-    print incl
+    # print incl 
     return sum(incl)
 
 
@@ -245,25 +255,49 @@ def Calc_Vars(M, n):
 
 if __name__ == '__main__':
 
+    '''
+    print 'building test data'
+
+    M_orig = []
+    for i in range(10):
+        row = np.random.rand(100)
+        row = [r/sum(row) for r in row]
+        M_orig.append(row)
+    M_orig = np.array(M_orig)
+
+    rs = 5
+    
+    candidates = [x for x in range(7)]
+
+    variances = []
+    for i in range(10):
+        row = np.random.rand(100)
+        row = [r/10000 for r in row]
+        variances.append(row)
+    variances = np.array(variances)
+
+    print Projection_Find(M_orig, rs, candidates, variances)
+    '''
+
     pass
 
-    # basis = np.array([[0.8, 0.1, 0.1], [0.1, 0.8, 0.1], [0.1, 0.1, 0.8]])
-    # # point = np.array([0.9, 0.05, 0.05])
-    # point = np.array([0.3, 0.3, 0.4])
-    # print in_conv_hull(basis, point)
-    # print res
-    # print np.linalg.norm(np.dot(res.x,basis)-point)**2
-    # print res.x
-    # print np.dot(res.x,basis)
-    # print np.linalg.norm(np.dot(res.x,basis)-point)**2 <= 1e-7
+    # # basis = np.array([[0.8, 0.1, 0.1], [0.1, 0.8, 0.1], [0.1, 0.1, 0.8]])
+    # # # point = np.array([0.9, 0.05, 0.05])
+    # # point = np.array([0.3, 0.3, 0.4])
+    # # print in_conv_hull(basis, point)
+    # # print res
+    # # print np.linalg.norm(np.dot(res.x,basis)-point)**2
+    # # print res.x
+    # # print np.dot(res.x,basis)
+    # # print np.linalg.norm(np.dot(res.x,basis)-point)**2 <= 1e-7
 
-    # print 'loading stored values', time.time()
+    # # print 'loading stored values', time.time()
 
-    # M = np.loadtxt('../result_out.20.M')
-    # candidates = np.loadtxt('../result_out.20.candidates')
-    # var = np.loadtxt('../result_out.20.var')
+    # # M = np.loadtxt('../result_out.20.M')
+    # # candidates = np.loadtxt('../result_out.20.candidates')
+    # # var = np.loadtxt('../result_out.20.var')
 
-    # print Projection_Find(M, 20, candidates, var)
+    # # print Projection_Find(M, 20, candidates, var)
 
 
 
